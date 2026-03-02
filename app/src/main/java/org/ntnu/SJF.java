efcd622335f6052c28b20e2727f1df558144a0e9
Jogeir Liavåg <jogeir@tutanota.com>
2026-03-02 15:33:33 +0100

    finish implementation

--- app/src/main/java/org/ntnu/SJF.java
+++ app/src/main/java/org/ntnu/SJF.java
@@ -1,5 +1,6 @@
 package org.ntnu;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SJF implements Strategy {
@@ -13,20 +14,41 @@ public class SJF implements Strategy {
   @Override
   public void execute() {
     boolean finished = false;
-    int[] turnaroundTimes;
-    int[] waitingTimes;
+    List<Integer> turnaroundTimes = new ArrayList<Integer>();
+    List<Integer> waitingTimes = new ArrayList<Integer>();
     System.out.println("Shortest Job First:\n" +
                       "========================================");
 
     while (!finished) {
       Process currentProcess = shortestAvailableJob();
       if (shortestAvailableJob() != null) {
+        int completionTime = totalTime + currentProcess.getRemainingBurstTime();
+        int turnaroundTime = completionTime - currentProcess.getArrivalTime();
+        int waitingTime = turnaroundTime - currentProcess.getRemainingBurstTime();
 
+        turnaroundTimes.add(turnaroundTime);
+        waitingTimes.add(waitingTime);
 
+        System.out.println(
+                            "Process " + currentProcess.getID() + "finished\n" +
+                            "Completion time = " + completionTime + "\n" +
+                            "Turnaround time = " + turnaroundTime + "\n" +
+                            "Waiting time = " + waitingTime + "\n"
+                          );
+        this.processes.remove(currentProcess);
       }
+      this.totalTime++;
 
-        this.totalTime++;
+      if (this.processes.isEmpty()) {
+        finished = true;
+      }
     }
+    double averageTurnaroundTime = turnaroundTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+    double averageWaitingTime = waitingTimes.stream().mapToInt(Integer::intValue).average().orElse(0.0);
+
+    System.out.println("Average turnaround time = " + averageTurnaroundTime);
+    System.out.println("Average waiting time = " + averageWaitingTime);
+    System.out.println("Total time spent = " + this.totalTime);
     System.out.println("========================================\n");
   }
 
