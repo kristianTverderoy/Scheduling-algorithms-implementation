13f713509634e80f95f89e4a045d198f02f420ef
Jogeir Liavåg <jogeir@tutanota.com>
2026-03-02 14:47:29 +0100

    unfinished implementation of SJF

--- app/src/main/java/org/ntnu/SJF.java
+++ app/src/main/java/org/ntnu/SJF.java
@@ -1,9 +1,47 @@
 package org.ntnu;
 
+import java.util.List;
+
 public class SJF implements Strategy {
+  private int totalTime = 0;
+  private List<Process> processes;
+
+  public SJF(List<Process> processes) {
+    this.processes = processes;
+  }
 
   @Override
   public void execute() {
+    boolean finished = false;
+    int[] turnaroundTimes;
+    int[] waitingTimes;
+    System.out.println("Shortest Job First:\n" +
+                      "========================================");
 
+    while (!finished) {
+      Process currentProcess = shortestAvailableJob();
+      if (shortestAvailableJob() != null) {
+
+
+      }
+
+        this.totalTime++;
+    }
+    System.out.println("========================================\n");
+  }
+
+
+  public Process shortestAvailableJob() {
+    Process shortestProcess = null;
+    for (Process process : this.processes) {
+      if (process.getArrivalTime() <= this.totalTime) {
+        if (shortestProcess == null
+          || shortestProcess.getRemainingBurstTime() > process.getRemainingBurstTime()) {
+
+          shortestProcess = process;
+        }
+      }
+    }
+    return shortestProcess;
   }
 }
